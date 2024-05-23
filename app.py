0d113797336a934834f1f0a3ea337e6df2b05b39
anujbohra23 <anujbohra10@gmail.com>
2024-05-23 15:52:34 +0530

    flask code for integration of a basic model

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        "index.html", prediction_text="Predicted employee salary is ${}".format(output)
+    )
+
+
+@app.route("/predict_api", methods=["POST"])  # for direct api calls through requests
+def predict_api():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
